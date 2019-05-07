32514d34b0ad7f2f0e4e4dec409faba9e629415f
yurchenkom <M.Yurchenko@tt.com.pl>
2019-05-07 15:13:53 +0200

    Fix dev mode

--- main.py
+++ main.py
@@ -3,50 +3,22 @@ from tkinter import messagebox
 
 import config
 from base_frame import BaseFrame
+from login import LoginFrame
 
 
 class AppFrame(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        self.frame = LoginFrame(self)
+        if config.DEV:
+            self.frame = MainFrame(self)
+        else:
+            self.frame = LoginFrame(self, MainFrame)
         self.frame.pack()
 
     def change(self, frame):
         self.frame.pack_forget()
         self.frame = frame(self)
         self.frame.pack()
-
-
-class LoginFrame(tk.Frame):
-    def __init__(self, master=None, **kwargs):
-        tk.Frame.__init__(self, master, **kwargs)
-
-        master.title(config.LOGIN_TITLE)
-        master.geometry("300x200+300+200")
-
-        lbl = tk.Label(self, text='Enter login')
-        lbl.pack()
-        self.login = tk.Entry(self, show="*")
-        self.login.pack()
-        self.login.focus()
-        self.login.bind('<Return>', self.check)
-
-        lbl = tk.Label(self, text='Enter password')
-        lbl.pack()
-        self.pwd = tk.Entry(self, show="*")
-        self.pwd.pack()
-        self.pwd.bind('<Return>', self.check)
-        btn = tk.Button(self, text="Done", command=self.check)
-        btn.pack()
-        btn = tk.Button(self, text="Cancel", command=self.quit)
-        btn.pack()
-
-    def check(self, event=None):
-        if self.pwd.get() == config.DEFAULT_PASSWORD and self.login.get() == config.DEFAULT_LOGIN:
-            messagebox.showinfo("Successfully login", "Welcome")
-            self.master.change(MainFrame)
-        else:
-            messagebox.showwarning("Login failed", "Please try again")
 
 
 class MainFrame(BaseFrame, tk.Frame):
